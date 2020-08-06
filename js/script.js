fb9470c2f5126b09333170d24890043109534908
Dev-Vladimir <63652808+Dev-Vladimir@users.noreply.github.com>
2020-08-06 23:16:12 +0300

    upload updatet files
    added other pages, fixed some css

--- js/script.js
+++ js/script.js
@@ -1,18 +1,8 @@
-// jQuery(document).scroll(function(){
-// 	let scroll = jQuery(window).scrollTop();
-// 		breakpoint1 = 0;
-// 		breakpoint2 = jQuery('.about-text').offset().top;
-// 		breakpoint3 = jQuery('.channel-article-text>a.w-light').offset().top;
-// 		breakpoint4 = jQuery('.popular').offset().top;
-// 	if (scroll > breakpoint1){lineParallax(jQuery('.about>.line'), scroll / 10)}
-// 	if (scroll > breakpoint2){lineParallax(jQuery('.channel .line'), (scroll - breakpoint2) / 5)}
-// 	if (scroll > breakpoint3){lineParallax(jQuery('.blog>.line'), (scroll - breakpoint3) / 5)}
-// 	if (scroll > breakpoint4){lineParallax(jQuery('.courses .line'), (scroll - breakpoint4) / 5)}
-
-// 	// lineParallax(line, scroll);
-
-// })
-
-// function lineParallax(line, scroll){
-// 	line.css({"transform": `translateY(${scroll}px)`})
-// }
+jQuery(document).ready(function(){
+	jQuery('.burger').click(function(){
+		jQuery('.nav').addClass('active');
+	})
+	jQuery('.close').click(function(){
+		jQuery('.nav').removeClass('active');
+	})
+})
