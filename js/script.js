e0d3196e5fe392c9cd77810b57b7774a3e0fc1a9
Dev-Vladimir <63652808+Dev-Vladimir@users.noreply.github.com>
2020-09-07 12:56:02 +0300

    Update files

--- js/script.js
+++ js/script.js
@@ -5,4 +5,16 @@ jQuery(document).ready(function(){
 	jQuery('.close').click(function(){
 		jQuery('.nav').removeClass('active');
 	})
+	jQuery(".courses-items>.item").click(function(){
+		let name = jQuery(this).attr('data-name');
+		showModal(name);
+	})
+	jQuery(".modal>.close").click(function(){
+		jQuery(this).parent().removeClass('active');
+	})
 })
+
+
+function showModal(name){
+	jQuery(`#${name}`).addClass('active');
+}
